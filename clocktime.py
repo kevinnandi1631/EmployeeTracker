020d928e335ee6fc2aa3e9780c926bf296bc8a15
kevinnandi1631 <kevinnandi1631@gmail.com>
2024-10-14 15:53:27 +0300

    Basic code for biometric clockin system for attendance

---
+++ clocktime.py
@@ -0,0 +1,17 @@
+import time
+
+time_in = 8
+
+clock_in = input("Enter the clock in time")
+
+putin = int(clock_in)
+
+if putin >= time_in:
+
+    print("Clocked in early")
+
+else:
+
+        print("Too late, go home")
+
+    
